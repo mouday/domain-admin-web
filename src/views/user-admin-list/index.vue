4679374676b7d05317abe58c1d18fbbe5025d418
pengshiyu <1940607002@qq.com>
2022-10-25 17:39:05 +0800

    fix

--- src/views/user-admin-list/index.vue
+++ src/views/user-admin-list/index.vue
@@ -12,7 +12,7 @@
         class="ml-sm"
         style="width: 260px"
         v-model="keyword"
-        placeholder="输入用户名"
+        placeholder="搜索用户名"
         clearable
         @keypress.enter="handleSearch"
         @clear="handleSearch"
