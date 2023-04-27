70a74d4312dc6a09745767403dbcfa4717e651e0
pengshiyu <1940607002@qq.com>
2023-04-27 10:15:43 +0800

    fix

--- src/views/domain-list/CheckDomainInfo.vue
+++ src/views/domain-list/CheckDomainInfo.vue
@@ -56,6 +56,9 @@ export default {
         if (res.data.status == false) {
           clearInterval(this.updateTimer)
           this.updateTimer = null
+          
+          this.$msg.success('操作成功')
+
           this.$emit('on-success')
         }
       }
