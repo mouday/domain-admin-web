773b0dad59cc19aac9ccc9ecefb6d75afb50e37f
pengshiyu <1940607002@qq.com>
2023-06-05 16:50:37 +0800

    fix

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -8,7 +8,12 @@
       <div class="app-layout__body">
         <!-- <div class="app-layout__path">{{ currentPathName }}</div> -->
         <div class="app-layout__view">
-          <router-view></router-view>
+          <transition
+            name="fade-transform"
+            mode="out-in"
+          >
+            <router-view></router-view>
+          </transition>
         </div>
       </div>
     </div>
@@ -85,5 +90,21 @@ export default {
 
 .app-layout__view {
   padding-top: 10px;
+}
+
+/* fade-transform */
+.fade-transform-leave-active,
+.fade-transform-enter-active {
+  transition: all .5s;
+}
+
+.fade-transform-enter {
+  opacity: 0;
+  transform: translateX(-30px);
+}
+
+.fade-transform-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
 }
 </style>
