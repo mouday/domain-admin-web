87616ef8c1e429a0020ec966e8a38d46a4a6b1ec
NietzscheX <libailua@gmail.com>
2023-08-03 17:14:05 +0800

    国际化翻译 (#3)
    * 国际化更新
    
    * 国际化更新
    
    ---------
    
    Co-authored-by: libai <libai@hongju.cc>

--- src/i18n/config.js
+++ src/i18n/config.js
@@ -9,7 +9,7 @@ export default [
   },
   {
     'zh-CN': '域名列表',
-    'en-US': 'Domain',
+    'en-US': 'Domain List',
   },
   {
     'zh-CN': '证书',
@@ -17,7 +17,7 @@ export default [
   },
   {
     'zh-CN': '证书列表',
-    'en-US': 'SSL Cert',
+    'en-US': 'Cert List',
   },
   {
     'zh-CN': '分组',
@@ -25,7 +25,7 @@ export default [
   },
   {
     'zh-CN': '分组管理',
-    'en-US': 'Group',
+    'en-US': 'Group Manage',
   },
   {
     'zh-CN': '通知',
@@ -33,21 +33,21 @@ export default [
   },
   {
     'zh-CN': '通知列表',
-    'en-US': 'Notify',
+    'en-US': 'Notify List',
   },
   {
     'zh-CN': '数据管理',
-    'en-US': 'Data',
+    'en-US': 'Data Manage',
   },
 
   {
     'zh-CN': '系统管理',
-    'en-US': 'System',
+    'en-US': 'System Manage',
   },
 
   {
     'zh-CN': '系统日志',
-    'en-US': 'Log',
+    'en-US': 'System Log',
   },
   {
     'zh-CN': '登录',
@@ -59,23 +59,23 @@ export default [
   },
   {
     'zh-CN': '全部证书',
-    'en-US': 'All Cert',
+    'en-US': 'All Certs',
   },
   {
     'zh-CN': '全部域名',
-    'en-US': 'All Domain',
+    'en-US': 'All Domains',
   },
   {
     'zh-CN': '全部分组',
-    'en-US': 'All Group',
+    'en-US': 'All Groups',
   },
   {
     'zh-CN': '用户管理',
-    'en-US': 'Users',
+    'en-US': 'Users Manage',
   },
   {
     'zh-CN': '系统设置',
-    'en-US': 'Settings',
+    'en-US': 'System Settings',
   },
   {
     'zh-CN': '监测日志',
@@ -87,7 +87,7 @@ export default [
   },
   {
     'zh-CN': '异步任务',
-    'en-US': 'Task Log',
+    'en-US': 'Async Task',
   },
 
   {
@@ -109,15 +109,15 @@ export default [
   },
   {
     'zh-CN': '域名状态',
-    'en-US': 'Status',
+    'en-US': 'Domain Status',
   },
   {
     'zh-CN': '证书状态',
-    'en-US': 'Status',
+    'en-US': 'Cert Status',
   },
   {
     'zh-CN': '域名分组',
-    'en-US': 'Group',
+    'en-US': 'Domain Group',
   },
   {
     'zh-CN': '添加',
