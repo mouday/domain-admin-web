e042c5cc883f1d137364a4ce5e5523ee592e6db0
NietzscheX <libailua@gmail.com>
2023-08-04 09:43:10 +0800

    国际化翻译 (#4)
    * 国际化更新
    
    * 国际化更新
    
    * 国际化更新新增
    
    * 国际化更新update
    
    ---------
    
    Co-authored-by: libai <libai@hongju.cc>

--- src/i18n/config.js
+++ src/i18n/config.js
@@ -39,12 +39,10 @@ export default [
     'zh-CN': '数据管理',
     'en-US': 'Data Manage',
   },
-
   {
     'zh-CN': '系统管理',
     'en-US': 'System Manage',
   },
-
   {
     'zh-CN': '系统日志',
     'en-US': 'System Log',
@@ -89,12 +87,10 @@ export default [
     'zh-CN': '异步任务',
     'en-US': 'Async Task',
   },
-
   {
     'zh-CN': '使用文档',
     'en-US': 'Document',
   },
-
   {
     'zh-CN': '语言',
     'en-US': 'Language',
@@ -123,4 +119,245 @@ export default [
     'zh-CN': '添加',
     'en-US': 'Add',
   },
+  {
+    'zh-CN': '不限',
+    'en-US': 'Any',
+  },
+  {
+    'zh-CN': '已过期',
+    'en-US': 'Expired',
+  },
+  {
+    'zh-CN': '3天内过期',
+    'en-US': 'in 3 days',
+  },
+  {
+    'zh-CN': '7天内过期',
+    'en-US': 'in 1 week',
+  },
+  {
+    'zh-CN': '30天内过期',
+    'en-US': 'in 1 month',
+  },
+  {
+    'zh-CN': '全部更新',
+    'en-US': 'Update All',
+  },
+  {
+    'zh-CN': '证书检查',
+    'en-US': 'Cert Check',
+  },
+  {
+    'zh-CN': '导入',
+    'en-US': 'Import',
+  },
+  {
+    'zh-CN': '导出',
+    'en-US': 'Export',
+  },
+  {
+    'zh-CN': '补全ICP',
+    'en-US': 'Complete ICP Information',
+  },
+  {
+    'zh-CN': '域名检查',
+    'en-US': 'Domain Check',
+  },
+  {
+    'zh-CN': '表格设置',
+    'en-US': 'Table Setting',
+  },
+  {
+    'zh-CN': '名称',
+    'en-US': 'Name',
+  },
+  {
+    'zh-CN': '证书数量',
+    'en-US': 'Cert Count',
+  },
+  {
+    'zh-CN': '域名数量',
+    'en-US': 'Domain Count',
+  },
+  {
+    'zh-CN': '成员数量',
+    'en-US': 'User Count',
+  },
+  {
+    'zh-CN': '关联证书',
+    'en-US': 'Cert Connection',
+  },
+  {
+    'zh-CN': '编辑',
+    'en-US': 'Edit',
+  },
+  {
+    'zh-CN': '删除',
+    'en-US': 'Delete',
+  },
+  {
+    'zh-CN': '序号',
+    'en-US': 'No.',
+  },
+  {
+    'zh-CN': '事件类型',
+    'en-US': 'Event Type',
+  },
+  {
+    'zh-CN': '通知方式',
+    'en-US': 'Notify',
+  },
+  {
+    'zh-CN': '剩余天数',
+    'en-US': 'Time of expiry',
+  },
+  {
+    'zh-CN': '备注',
+    'en-US': 'Comment',
+  },
+  {
+    'zh-CN': '启用',
+    'en-US': 'Turn On',
+  },
+  {
+    'zh-CN': '测试',
+    'en-US': 'Try Test',
+  },
+  {
+    'zh-CN': '主机数量',
+    'en-US': 'Host Count',
+  },
+  {
+    'zh-CN': '证书天数',
+    'en-US': 'Time of Cert expiry',
+  },
+  {
+    'zh-CN': '更新时间',
+    'en-US': 'Update Time',
+  },
+  {
+    'zh-CN': '操作',
+    'en-US': 'Operate',
+  },
+  {
+    'zh-CN': '域名天数',
+    'en-US': 'Time of Domain expiry',
+  },
+  {
+    'zh-CN': '证书数量',
+    'en-US': 'Cert Count',
+  },
+  {
+    'zh-CN': '标签',
+    'en-US': 'Lable',
+  },
+  {
+    'zh-CN': '负责人',
+    'en-US': 'Owner',
+  },
+  {
+    'zh-CN': '用户名',
+    'en-US': 'User Name',
+  },
+  {
+    'zh-CN': '分组数量',
+    'en-US': 'Group Count',
+  },
+  {
+    'zh-CN': '通知渠道',
+    'en-US': 'Notify By',
+  },
+  {
+    'zh-CN': '重置密码',
+    'en-US': 'Reset Password',
+  },
+  {
+    'zh-CN': '邮件设置',
+    'en-US': 'Email Setting',
+  },
+  {
+    'zh-CN': '监测设置',
+    'en-US': 'Moniter Setting',
+  },
+  {
+    'zh-CN': '服务器地址',
+    'en-US': 'Server Address',
+  },
+  {
+    'zh-CN': '服务器端口',
+    'en-US': 'Server Port',
+  },
+  {
+    'zh-CN': '发件人名称',
+    'en-US': 'Sender Name',
+  },
+  {
+    'zh-CN': '发件人账号',
+    'en-US': 'Sender Account',
+  },
+  {
+    'zh-CN': '发件人密码',
+    'en-US': 'Sender Password',
+  },
+  {
+    'zh-CN': '保存',
+    'en-US': 'Save',
+  },
+  {
+    'zh-CN': '定时设置',
+    'en-US': 'Cron Setting',
+  },
+  {
+    'zh-CN': '执行计划',
+    'en-US': 'Cron Plan',
+  },
+  {
+    'zh-CN': '开始时间',
+    'en-US': 'Start Time',
+  },
+  {
+    'zh-CN': '结束时间',
+    'en-US': 'End Time',
+  },
+  {
+    'zh-CN': '持续时间',
+    'en-US': 'Duration',
+  },
+  {
+    'zh-CN': '错误信息',
+    'en-US': 'Error Info',
+  },
+  {
+    'zh-CN': '清空日志',
+    'en-US': 'Delete Log',
+  },
+  {
+    'zh-CN': '操作人',
+    'en-US': 'User of Operate',
+  },
+  {
+    'zh-CN': '操作表',
+    'en-US': 'Table of Operate',
+  },
+  {
+    'zh-CN': '操作类型',
+    'en-US': 'Type of Operate',
+  },
+  {
+    'zh-CN': '数据变化',
+    'en-US': 'Data Change',
+  },
+  {
+    'zh-CN': '操作时间',
+    'en-US': 'Time of Operate',
+  },
+  {
+    'zh-CN': '任务名称',
+    'en-US': 'Task Name',
+  },
+  {
+    'zh-CN': '执行结果',
+    'en-US': 'Excute Result',
+  },
+
 ]
