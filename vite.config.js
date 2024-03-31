09a659837ced2d6316eb42f7a796af8ae13f6153
pengshiyu <1940607002@qq.com>
2024-03-31 21:20:32 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import { visualizer } from 'rollup-plugin-visualizer'
 // import viteCompression from 'vite-plugin-compression'
 import { CodeInspectorPlugin } from 'code-inspector-plugin'
 
+// eslint-disable-next-line no-control-regex
+const INVALID_CHAR_REGEX = /[\u0000-\u001F"#$&*+,:;<=>?[\]^`{|}\u007F]/g
+const DRIVE_LETTER_REGEX = /^[a-z]:/i
+
 // https://vitejs.dev/config/
 export default defineConfig((config) => {
   console.log(config)
@@ -65,6 +69,17 @@ export default defineConfig((config) => {
               'mockjs',
               'lodash',
             ],
+          },
+          // https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
+          sanitizeFileName(name) {
+            const match = DRIVE_LETTER_REGEX.exec(name)
+            const driveLetter = match ? match[0] : ''
+            // A `:` is only allowed as part of a windows drive letter (ex: C:\foo)
+            // Otherwise, avoid them because they can refer to NTFS alternate data streams.
+            return (
+              driveLetter +
+              name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, '')
+            )
           },
         },
       },
