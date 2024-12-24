5e8af273adfda58dbc35cfd80ce3724f860df3b8
aaronldevops <153443496+aaronldevops@users.noreply.github.com>
2024-12-24 10:05:07 +0800

    add zh TW

--- src/i18n/config.js
+++ src/i18n/config.js
@@ -2,668 +2,819 @@ export default [
   {
     'zh-CN': '简体中文',
     'en-US': 'English',
+    'zh-TW': '繁體中文',
   },
   {
     'zh-CN': '域名',
     'en-US': 'Domain',
+    'zh-TW': '域名',
   },
   {
     'zh-CN': '域名列表',
     'en-US': 'Domain List',
+    'zh-TW': '域名列表',
   },
   {
     'zh-CN': '证书',
     'en-US': 'Cert',
+    'zh-TW': '證書',
   },
   {
     'zh-CN': '证书列表',
     'en-US': 'Cert List',
+    'zh-TW': '證書列表',
   },
   {
     'zh-CN': '分组',
     'en-US': 'Group',
+    'zh-TW': '群組',
   },
   {
     'zh-CN': '分组管理',
     'en-US': 'Group Manage',
+    'zh-TW': '群組管理',
   },
   {
     'zh-CN': '通知',
     'en-US': 'Notify',
+    'zh-TW': '通知',
   },
   {
     'zh-CN': '通知列表',
     'en-US': 'Notify List',
+    'zh-TW': '通知列表',
   },
   {
     'zh-CN': '添加通知',
     'en-US': 'Add Notify',
+    'zh-TW': '新增通知',
   },
   {
     'zh-CN': '编辑通知',
     'en-US': 'Edit Notify',
+    'zh-TW': '編輯通知',
   },
   {
     'zh-CN': '数据管理',
     'en-US': 'Data Manage',
+    'zh-TW': '資料管理',
   },
   {
     'zh-CN': '系统管理',
     'en-US': 'System Manage',
+    'zh-TW': '系統管理',
   },
   {
     'zh-CN': '系统日志',
     'en-US': 'System Log',
+    'zh-TW': '系統日誌',
   },
   {
     'zh-CN': '登录',
     'en-US': 'Login',
+    'zh-TW': '登錄',
   },
   {
     'zh-CN': '工具箱',
     'en-US': 'Tools',
+    'zh-TW': '工具箱',
   },
   {
     'zh-CN': '全部证书',
     'en-US': 'All Certs',
+    'zh-TW': '全部證書',
   },
   {
     'zh-CN': '全部域名',
     'en-US': 'All Domains',
+    'zh-TW': '全部域名',
   },
   {
     'zh-CN': '全部分组',
     'en-US': 'All Groups',
+    'zh-TW': '全部群組',
   },
   {
     'zh-CN': '用户管理',
     'en-US': 'Users Manage',
+    'zh-TW': '用戶管理',
   },
   {
     'zh-CN': '系统设置',
     'en-US': 'System Settings',
+    'zh-TW': '系統設置',
   },
   {
     'zh-CN': '监测日志',
     'en-US': 'Monitor Log',
+    'zh-TW': '監控日誌',
   },
   {
     'zh-CN': '操作日志',
     'en-US': 'Operate Log',
+    'zh-TW': '操作日誌',
   },
   {
     'zh-CN': '异步任务',
     'en-US': 'Async Task',
+    'zh-TW': '非同步任務',
   },
   {
     'zh-CN': '使用文档',
     'en-US': 'Document',
+    'zh-TW': '使用文檔',
   },
   {
     'zh-CN': '语言',
     'en-US': 'Language',
+    'zh-TW': '語言',
   },
   {
     'zh-CN': '经典',
     'en-US': 'Blue',
+    'zh-TW': '經典',
   },
   {
     'zh-CN': '极客',
     'en-US': 'Black',
+    'zh-TW': '黑色',
   },
   {
     'zh-CN': '域名状态',
     'en-US': 'Domain Status',
+    'zh-TW': '域名狀態',
   },
   {
     'zh-CN': '证书状态',
     'en-US': 'Cert Status',
+    'zh-TW': '證書狀態',
   },
   {
     'zh-CN': '域名分组',
     'en-US': 'Domain Group',
+    'zh-TW': '域名群組',
   },
   {
     'zh-CN': '添加',
     'en-US': 'Add',
+    'zh-TW': '新增',
   },
   {
     'zh-CN': '不限',
     'en-US': 'Any',
+    'zh-TW': '不限',
   },
   {
     'zh-CN': '已过期',
     'en-US': 'Expired',
+    'zh-TW': '已過期',
   },
   {
     'zh-CN': '3天内过期',
     'en-US': 'in 3 days',
+    'zh-TW': '3天內過期',
   },
   {
     'zh-CN': '7天内过期',
     'en-US': 'in 1 week',
+    'zh-TW': '7天內過期',
   },
   {
     'zh-CN': '30天内过期',
     'en-US': 'in 1 month',
+    'zh-TW': '30天內過期',
   },
   {
     'zh-CN': '全部更新',
     'en-US': 'Update All',
+    'zh-TW': '全部更新',
   },
   {
     'zh-CN': '证书检查',
     'en-US': 'Cert Check',
+    'zh-TW': '證書檢查',
   },
   {
     'zh-CN': '导入',
     'en-US': 'Import',
+    'zh-TW': '上傳',
   },
   {
     'zh-CN': '导出',
     'en-US': 'Export',
+    'zh-TW': '輸出',
   },
   {
     'zh-CN': '补全ICP',
     'en-US': 'Complete ICP',
+    'zh-TW': '補全ICP',
   },
   {
     'zh-CN': '域名检查',
     'en-US': 'Domain Check',
+    'zh-TW': '域名檢查',
   },
   {
     'zh-CN': '表格设置',
     'en-US': 'Table Setting',
+    'zh-TW': '表格設置',
   },
   {
     'zh-CN': '显示字段',
     'en-US': 'Fields',
+    'zh-TW': '顯示欄位',
   },
   {
     'zh-CN': '名称',
     'en-US': 'Name',
+    'zh-TW': '名稱',
   },
   {
     'zh-CN': '证书数量',
     'en-US': 'Cert Count',
+    'zh-TW': '證書數量',
   },
   {
     'zh-CN': '过期证书',
     'en-US': 'Expired Cert',
+    'zh-TW': '過期證書',
   },
   {
     'zh-CN': '过期域名',
     'en-US': 'Expired Domain',
-  },
-  {
-    'zh-CN': '过期域名',
-    'en-US': 'Expired Domain',
+    'zh-TW': '過期域名',
   },
   {
     'zh-CN': '网站异常',
     'en-US': 'Web Error',
+    'zh-TW': '網站異常',
   },
   {
     'zh-CN': '网站数量',
     'en-US': 'Web Count',
+    'zh-TW': '網站數量',
   },
   {
     'zh-CN': '域名数量',
     'en-US': 'Domain Count',
+    'zh-TW': '域名數量',
   },
   {
     'zh-CN': '成员数量',
     'en-US': 'User Count',
+    'zh-TW': '用戶數量',
   },
   {
     'zh-CN': '关联证书',
     'en-US': 'Relation Cert',
+    'zh-TW': '關聯證書',
   },
   {
     'zh-CN': '编辑',
     'en-US': 'Edit',
+    'zh-TW': '編輯',
   },
   {
     'zh-CN': '删除',
     'en-US': 'Delete',
+    'zh-TW': '刪除',
   },
   {
     'zh-CN': '序号',
     'en-US': 'No.',
+    'zh-TW': '序號',
   },
   {
     'zh-CN': '事件类型',
     'en-US': 'Event',
+    'zh-TW': '事件類型',
   },
   {
     'zh-CN': '通知方式',
     'en-US': 'Notify',
+    'zh-TW': '通知方式',
   },
   {
     'zh-CN': '剩余天数',
     'en-US': 'Days',
+    'zh-TW': '剩餘天數',
   },
   {
     'zh-CN': '备注',
     'en-US': 'Comment',
+    'zh-TW': '備注',
   },
   {
     'zh-CN': '启用',
     'en-US': 'Enabled',
+    'zh-TW': '啟用',
   },
   {
     'zh-CN': '测试',
     'en-US': 'Test',
+    'zh-TW': '測試',
   },
   {
     'zh-CN': '主机数量',
     'en-US': 'Hosts',
+    'zh-TW': '主機數量',
   },
   {
     'zh-CN': '证书天数',
     'en-US': 'Days',
+    'zh-TW': '證書天數',
   },
   {
     'zh-CN': '更新时间',
     'en-US': 'Updated',
+    'zh-TW': '更新時間',
   },
   {
     'zh-CN': '操作',
     'en-US': 'Operate',
+    'zh-TW': '操作',
   },
   {
     'zh-CN': '域名天数',
     'en-US': 'Days',
+    'zh-TW': '域名天數',
   },
   {
     'zh-CN': '证书数量',
     'en-US': 'Cert Count',
+    'zh-TW': '證書數量',
   },
   {
     'zh-CN': '标签',
     'en-US': 'Tags',
+    'zh-TW': '標籤',
   },
   {
     'zh-CN': '负责人',
     'en-US': 'Owner',
+    'zh-TW': '負責人',
   },
   {
     'zh-CN': '用户名',
     'en-US': 'User Name',
+    'zh-TW': '使用者名稱',
   },
   {
     'zh-CN': '用户',
     'en-US': 'User',
+    'zh-TW': '使用者',
   },
   {
     'zh-CN': '请输入用户名',
     'en-US': 'Input User',
+    'zh-TW': '請輸入使用者名',
   },
   {
     'zh-CN': '分组数量',
     'en-US': 'Group Count',
+    'zh-TW': '群組數量',
   },
   {
     'zh-CN': '通知渠道',
     'en-US': 'Notify',
+    'zh-TW': '通知管道',
   },
   {
     'zh-CN': '重置密码',
     'en-US': 'Reset Password',
+    'zh-TW': '重置密碼',
   },
   {
     'zh-CN': '密码',
     'en-US': 'Password',
+    'zh-TW': '密碼',
   },
   {
     'zh-CN': '请输入密码',
     'en-US': 'Input Password',
+    'zh-TW': '請輸入密碼',
   },
   {
     'zh-CN': '邮件设置',
     'en-US': 'Email Setting',
+    'zh-TW': '郵件設置',
   },
   {
     'zh-CN': '监测设置',
     'en-US': 'Moniter Setting',
+    'zh-TW': '監控設置',
   },
   {
     'zh-CN': '服务器地址',
     'en-US': 'Mail Host',
+    'zh-TW': '郵件主機',
   },
   {
     'zh-CN': '服务器端口',
     'en-US': 'Server Port',
+    'zh-TW': '伺服器 Port',
   },
   {
     'zh-CN': '发件人名称',
     'en-US': 'Sender Name',
+    'zh-TW': '發件人名稱',
   },
   {
     'zh-CN': '发件人账号',
     'en-US': 'Account',
+    'zh-TW': '發件人帳號',
   },
   {
     'zh-CN': '发件人密码',
     'en-US': 'Password',
+    'zh-TW': '發件人密碼',
   },
   {
     'zh-CN': '保存',
     'en-US': 'Save',
+    'zh-TW': '存檔',
   },
   {
     'zh-CN': '定时设置',
     'en-US': 'Cron Exp',
+    'zh-TW': '定時設置',
   },
   {
     'zh-CN': '执行计划',
     'en-US': 'Cron Plan',
+    'zh-TW': '定時任務',
   },
   {
     'zh-CN': '开始时间',
     'en-US': 'Start Time',
+    'zh-TW': '開始時間',
   },
   {
     'zh-CN': '结束时间',
     'en-US': 'End Time',
+    'zh-TW': '結束時間',
   },
   {
     'zh-CN': '持续时间',
     'en-US': 'Duration',
+    'zh-TW': '持續時間',
   },
   {
     'zh-CN': '错误信息',
     'en-US': 'Error Info',
+    'zh-TW': '錯誤資訊',
   },
   {
     'zh-CN': '清空日志',
     'en-US': 'Clear Log',
+    'zh-TW': '清空日誌',
   },
   {
     'zh-CN': '日志',
     'en-US': 'Log',
+    'zh-TW': '日誌',
   },
   {
     'zh-CN': '操作人',
     'en-US': 'User',
+    'zh-TW': '操作人',
   },
   {
     'zh-CN': '操作表',
     'en-US': 'Table',
+    'zh-TW': '操作表格',
   },
   {
     'zh-CN': '操作类型',
     'en-US': 'Operate',
+    'zh-TW': '操作類型',
   },
   {
     'zh-CN': '数据变化',
     'en-US': 'Data Change',
+    'zh-TW': '資料變化',
   },
   {
     'zh-CN': '操作时间',
     'en-US': 'Time of Operate',
+    'zh-TW': '操作時間',
   },
   {
     'zh-CN': '任务名称',
     'en-US': 'Task Name',
+    'zh-TW': '任務名稱',
   },
   {
     'zh-CN': '执行结果',
     'en-US': 'Excute Result',
+    'zh-TW': '執行結果',
   },
 
   {
     'zh-CN': '状态',
     'en-US': 'Status',
+    'zh-TW': '狀態',
   },
   {
     'zh-CN': '自动更新',
     'en-US': 'Auto Update',
+    'zh-TW': '自動更新',
   },
   {
     'zh-CN': '到期提醒',
     'en-US': 'Notify',
+    'zh-TW': '到期提醒',
   },
   {
     'zh-CN': '共计',
     'en-US': 'Total',
+    'zh-TW': '總計',
   },
   {
     'zh-CN': '条数据',
     'en-US': 'Rows',
+    'zh-TW': '筆資料',
   },
   {
     'zh-CN': '搜索域名',
     'en-US': 'Search Domain',
+    'zh-TW': '搜尋域名',
   },
 
   {
     'zh-CN': '搜索域名、标签',
     'en-US': 'Search Domain Or Tag',
+    'zh-TW': '搜尋域名、標籤',
   },
 
   {
     'zh-CN': '搜索用户名',
     'en-US': 'Search Username',
+    'zh-TW': '搜尋用戶名稱',
   },
   {
     'zh-CN': '搜索主机',
     'en-US': 'Search Host',
+    'zh-TW': '搜尋主機',
   },
   {
     'zh-CN': '搜索分组',
     'en-US': 'Search Group',
+    'zh-TW': '搜尋群組',
   },
   {
     'zh-CN': 'SSL证书到期',
     'en-US': 'SSL Cert Expired',
+    'zh-TW': 'SSL證書過期',
   },
   {
     'zh-CN': '域名到期',
     'en-US': 'Domain Expired',
+    'zh-TW': '域名過期',
   },
 
   {
     'zh-CN': '邮件',
     'en-US': 'Email',
+    'zh-TW': '信箱',
   },
-
   {
     'zh-CN': '企业微信',
     'en-US': 'WeCom',
+    'zh-TW': '企業微信',
   },
   {
     'zh-CN': '钉钉',
     'en-US': 'DingTalk',
+    'zh-TW': '釘釘',
   },
   {
     'zh-CN': '飞书',
     'en-US': 'Feishu',
+    'zh-TW': '飛書',
   },
   {
     'zh-CN': '触发事件',
     'en-US': 'Event',
+    'zh-TW': '觸發事件',
   },
   {
     'zh-CN': '邮件列表',
     'en-US': 'Receivers',
+    'zh-TW': '信箱列表',
   },
   {
     'zh-CN': '取消',
     'en-US': 'Cancel',
+    'zh-TW': '取消',
   },
   {
     'zh-CN': '确定删除',
     'en-US': 'Confirm Delete',
+    'zh-TW': '確認刪除',
   },
   {
     'zh-CN': '确定',
     'en-US': 'Confirm',
+    'zh-TW': '確認',
   },
   {
     'zh-CN': '添加域名',
     'en-US': 'Add Domain',
+    'zh-TW': '新增域名',
   },
   {
     'zh-CN': '编辑域名',
     'en-US': 'Edit Domain',
+    'zh-TW': '修改域名',
   },
 
   {
     'zh-CN': '证书时间',
     'en-US': 'Cert Time',
+    'zh-TW': '證書時間',
   },
   {
     'zh-CN': '端口',
     'en-US': 'Port',
+    'zh-TW': 'Port',
   },
   {
     'zh-CN': '动态主机',
     'en-US': 'Dynamic',
+    'zh-TW': '動態主機',
   },
   {
     'zh-CN': '证书颁发时间',
     'en-US': 'Start Time',
+    'zh-TW': '證書頒發時間',
   },
-
   {
     'zh-CN': '证书过期时间',
     'en-US': 'Expired Time',
+    'zh-TW': '證書到期時間',
   },
-
   {
     'zh-CN': '请输入域名',
     'en-US': 'Please Input Domain',
+    'zh-TW': '請輸入域名',
   },
   {
     'zh-CN': '请输入域名列表，每行一个',
     'en-US': 'Please enter a list of domain names, one per line',
+    'zh-TW': '請輸入域名列表，每行一個',
   },
   {
     'zh-CN': '请输入备注',
     'en-US': 'Please Input Comment',
+    'zh-TW': '請輸入備注',
   },
-
   {
     'zh-CN': '域名时间',
     'en-US': 'Domain Time',
+    'zh-TW': '域名時間',
   },
-
   {
     'zh-CN': '子域证书',
     'en-US': 'Sub Cert',
+    'zh-TW': '子域名證書',
   },
-
   {
     'zh-CN': '域名注册时间',
     'en-US': 'Start Time',
+    'zh-TW': '域名註冊時間',
   },
-
   {
     'zh-CN': '域名到期时间',
     'en-US': 'Expired Time',
+    'zh-TW': '域名過期時間',
   },
   {
     'zh-CN': '标签，回车确认',
     'en-US': 'Input Tag, Press Enter',
+    'zh-TW': '標籤, Enter確認',
   },
   {
     'zh-CN': '请输入主办单位名称',
     'en-US': 'Please Input Company Name',
+    'zh-TW': '請輸入公司名稱',
   },
   {
     'zh-CN': '主办单位名称',
     'en-US': 'Company Name',
+    'zh-TW': '公司名稱',
   },
   {
     'zh-CN': '主办单位',
     'en-US': 'Company',
+    'zh-TW': '公司',
   },
   {
     'zh-CN': 'ICP备案',
     'en-US': 'ICP',
+    'zh-TW': 'ICP備案',
   },
   {
     'zh-CN': '更新ICP',
     'en-US': 'Update ICP',
+    'zh-TW': '更新ICP',
   },
   {
     'zh-CN': '请输入ICP备案',
     'en-US': 'Please Input ICP Information',
+    'zh-TW': '請輸入ICP備案',
   },
 
   {
     'zh-CN': '操作成功',
     'en-US': 'Success',
+    'zh-TW': '操作成功',
   },
   {
     'zh-CN': 'SSL证书申请',
     'en-US': 'Get SSL Cert',
+    'zh-TW': 'SSL證書申請',
   },
   {
     'zh-CN': '申请',
     'en-US': 'Get',
+    'zh-TW': '申請',
   },
   {
     'zh-CN': 'SSL签发时间',
     'en-US': 'Start Time',
+    'zh-TW': 'SSL簽發時間',
   },
   {
     'zh-CN': 'SSL过期时间',
     'en-US': 'Expired Time',
+    'zh-TW': 'SSL過期時間',
   },
   {
     'zh-CN': '自动续期',
     'en-US': 'Auto Renew',
+    'zh-TW': '自動展期',
   },
   {
     'zh-CN': '查看',
     'en-US': 'Detail',
+    'zh-TW': '詳細資訊',
   },
   {
     'zh-CN': '续期',
     'en-US': 'Renew',
+    'zh-TW': '展期',
   },
   {
     'zh-CN': '未知',
     'en-US': 'unknown',
+    'zh-TW': '未知',
   },
   {
     'zh-CN': '已验证',
     'en-US': 'verified',
+    'zh-TW': '已驗證',
   },
   {
     'zh-CN': '未验证',
     'en-US': 'Not verified',
+    'zh-TW': '未驗證',
   },
   {
     'zh-CN': '验证',
     'en-US': 'verify',
+    'zh-TW': '驗證',
   },
 
   {
     'zh-CN': '是',
     'en-US': 'Yes',
+    'zh-TW': '是',
   },
   {
     'zh-CN': '否',
     'en-US': 'No',
+    'zh-TW': '否',
   },
   {
     'zh-CN': '申请SSL证书',
     'en-US': 'Get SSL Cert',
+    'zh-TW': '申請SSL證書',
   },
   {
     'zh-CN': 'SSL证书',
     'en-US': 'SSL Cert',
+    'zh-TW': 'SSL證書',
   },
   {
     'zh-CN': '填写域名',
     'en-US': 'Submit Domain',
+    'zh-TW': '填寫域名',
   },
   {
     'zh-CN': '验证域名',
     'en-US': 'Verify Domain',
+    'zh-TW': '驗件域名',
   },
   {
     'zh-CN': '下载证书',
@@ -677,197 +828,241 @@ export default [
   {
     'zh-CN': '文件验证',
     'en-US': 'File Verify',
+    'zh-TW': '文件驗證',
   },
   {
     'zh-CN': 'DNS验证',
     'en-US': 'DNS Verify',
+    'zh-TW': 'DNS驗證',
   },
   {
     'zh-CN': '验证状态',
     'en-US': 'Status',
+    'zh-TW': '驗證狀態',
   },
   {
     'zh-CN': '验证URL',
     'en-US': 'Verify URL',
+    'zh-TW': '驗證URL',
   },
   {
     'zh-CN': '验证文件',
     'en-US': 'Verify File',
+    'zh-TW': '驗證文件',
   },
   {
     'zh-CN': '服务器地址',
     'en-US': 'Server Host',
+    'zh-TW': '伺服器地址',
   },
   {
     'zh-CN': '服务器目录',
     'en-US': 'Server Directory',
+    'zh-TW': '伺服器目錄',
   },
   {
     'zh-CN': '下载',
     'en-US': 'Download',
+    'zh-TW': '下載',
   },
   {
     'zh-CN': 'Nginx配置',
     'en-US': 'Nginx Config',
+    'zh-TW': 'Nginx配置',
   },
   {
     'zh-CN': '重启命令',
     'en-US': 'Reload Command',
+    'zh-TW': '重啟指令',
   },
   {
     'zh-CN': '私钥部署路径',
     'en-US': 'Private Deploy Path',
+    'zh-TW': '私鑰部署路徑',
   },
   {
     'zh-CN': '公钥部署路径',
     'en-US': 'Public Deploy Path',
+    'zh-TW': '公鑰部署路徑',
   },
   {
     'zh-CN': '关闭',
     'en-US': 'Close',
+    'zh-TW': '關閉',
   },
   {
     'zh-CN': '一键部署',
-    'en-US': 'Auto Deploy',
+    'en-US': 'Auto Deploy'
+    'zh-TW': '一鍵部署',
   },
   {
     'zh-CN': '添加分组',
     'en-US': 'Add Group',
+    'zh-TW': '新增群組',
   },
   {
     'zh-CN': '编辑分组',
     'en-US': 'Edit Group',
+    'zh-TW': '編輯群組',
   },
   {
     'zh-CN': '名称',
     'en-US': 'Name',
+    'zh-TW': '名稱',
   },
   {
     'zh-CN': '请输入名称',
     'en-US': 'Input Name',
+    'zh-TW': '請輸入名稱',
   },
   {
     'zh-CN': '添加用户',
     'en-US': 'Add User',
+    'zh-TW': '新增用戶',
   },
   {
     'zh-CN': '编辑用户',
     'en-US': 'Edit User',
+    'zh-TW': '編輯用戶',
   },
   {
     'zh-CN': '证书监控',
     'en-US': 'Cert Monitor',
+    'zh-TW': '證書監控',
   },
   {
     'zh-CN': '证书申请',
     'en-US': 'Cert Issue',
+    'zh-TW': '證書申請',
   },
   {
     'zh-CN': '加密方式',
     'en-US': 'SSL Type',
+    'zh-TW': '加密方式',
   },
   {
     'zh-CN': '域名监控',
     'en-US': 'Domain Monitor',
+    'zh-TW': '域名統計',
   },
   {
     'zh-CN': '网站监控',
     'en-US': 'Web Monitor',
+    'zh-TW': '網站監控',
   },
   {
     'zh-CN': '通知管理',
     'en-US': 'Notify Manage',
+    'zh-TW': '通知管理',
   },
   {
     'zh-CN': '退出',
     'en-US': 'Logout',
+    'zh-TW': '登出',
   },
   {
     'zh-CN': '关于',
     'en-US': 'About',
+    'zh-TW': '關於',
   },
   {
     'zh-CN': '修改密码',
     'en-US': 'Change Password',
+    'zh-TW': '修改密碼',
   },
   {
     'zh-CN': '修改登录密码',
     'en-US': 'Change Password',
+    'zh-TW': '修改登錄密碼',
   },
   {
     'zh-CN': '旧密码',
     'en-US': 'Old Password',
+    'zh-TW': '舊密碼',
   },
   {
     'zh-CN': '新密码',
     'en-US': 'New Password',
+    'zh-TW': '新密碼',
   },
   {
     'zh-CN': '首页',
     'en-US': 'Index',
+    'zh-TW': '首頁',
   },
   {
     'zh-CN': '控制台',
     'en-US': 'Dashboard',
+    'zh-TW': '儀表板',
   },
   {
     'zh-CN': '请及时修改 admin（超级管理员）的默认密码',
     'en-US': 'Please Change default password of admin(super manager)',
+    'zh-TW': '請即時修改 admin (超級管理員) 的默認密碼',
   },
   {
     'zh-CN': '系统数据',
     'en-US': 'System Data',
+    'zh-TW': '系統數據',
   },
   {
     'zh-CN': '证书统计',
     'en-US': 'SSL Cert Data',
+    'zh-TW': '證書統計',
   },
   {
     'zh-CN': '域名统计',
     'en-US': 'Domain Data',
+    'zh-TW': '域名統計',
   },
   {
     'zh-CN': '监控统计',
     'en-US': 'Monitor Data',
-  },
-  {
-    'zh-CN': '监控统计',
-    'en-US': 'Monitor Data',
+    'zh-TW': '監控統計',
   },
   {
     'zh-CN': '未过期',
     'en-US': 'Ok',
+    'zh-TW': '未過期',
   },
   {
     'zh-CN': '即将过期',
     'en-US': 'Will Expired',
+    'zh-TW': '即將過期',
   },
   {
     'zh-CN': '正常',
     'en-US': 'Ok',
+    'zh-TW': '正常',
   },
   {
     'zh-CN': '异常',
     'en-US': 'Error',
+    'zh-TW': '錯誤',
   },
   {
     'zh-CN': '证书托管',
     'en-US': 'Cert Saver',
+    'zh-TW': '證書託管',
   },
   {
     'zh-CN': '网站状态',
     'en-US': 'Web Status',
+    'zh-TW': '網站狀態',
   },
   {
     'zh-CN': '下次运行时间',
     'en-US': 'Next Run Time',
+    'zh-TW': '下次執行時間',
   },
   {
     'zh-CN': '频率(分钟)',
     'en-US': 'Frequency(min)',
+    'zh-TW': '頻率(分鐘)',
   },
   {
     'zh-CN': '监控类型',
     'en-US': 'Type',
+    'zh-TW': '監控類型',
   },
 ]
